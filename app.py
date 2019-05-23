0d78b1a568f64743e081d03fc9b887504d409c7e
OtherThing <fivos.kavassalis@gmail.com>
2019-05-23 01:31:26 -0400

    'testing...'

---
+++ app.py
@@ -0,0 +1,18 @@
+# print("Fivos Kavassalis")
+# print("o----")
+# print(" ||||")
+# print("*" * 10)
+
+# age = 20
+# rating = 4.9
+# name = "Fivos Kavassalis"
+# is_published = True
+# print(is_published)
+#
+# name = input("What is your name? ")
+# print("Hi " + name + "!")
+
+# name = input("What is your name? ")
+# color = input("What is your fav color? ")
+# print(name + " likes " + color)
+
